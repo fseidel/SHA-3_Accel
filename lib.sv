a70bfdfb7f5b2850d5239f4bcfac14ceccbbfb81
Adilet Segizekov <asegizek@ece015.ece.local.cmu.edu>
2018-12-04 18:10:41 -0500

    added round constant function

--- lib.sv
+++ lib.sv
@@ -135,3 +135,34 @@ function logic [2:0] rho_pi_x
 
     endcase
 endfunction: rho_pi_x
+
+function logic [63:0] roundc(input logic [4:0] round);
+
+    unique case (round)
+        'd0: roundc = 64'h0000000000000001;
+        'd1: roundc = 64'h0000000000008082;
+        'd2: roundc = 64'h800000000000808A;
+        'd3: roundc = 64'h8000000080008000;
+        'd4: roundc = 64'h000000000000808B;
+        'd5: roundc = 64'h0000000080000001;
+        'd6: roundc = 64'h8000000080008081;
+        'd7: roundc = 64'h8000000000008009;
+        'd8: roundc = 64'h000000000000008A;
+        'd9: roundc = 64'h0000000000000088;
+        'd10: roundc = 64'h0000000080008009;
+        'd11: roundc = 64'h000000008000000A;
+        'd12: roundc = 64'h000000008000808B;
+        'd13: roundc = 64'h800000000000008B;
+        'd14: roundc = 64'h8000000000008089;
+        'd15: roundc = 64'h8000000000008003;
+        'd16: roundc = 64'h8000000000008002;
+        'd17: roundc = 64'h8000000000000080;
+        'd18: roundc = 64'h000000000000800A;
+        'd19: roundc = 64'h800000008000000A;
+        'd20: roundc = 64'h8000000080008081;
+        'd21: roundc = 64'h8000000000008080;
+        'd22: roundc = 64'h0000000080000001;
+        'd23: roundc = 64'h8000000080008008;
+    endcase
+
+endfunction: roundc
