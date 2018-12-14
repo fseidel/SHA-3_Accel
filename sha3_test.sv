f4a8ed43d8731f82ce78937a3c640146a2d3887e
Adilet Segizekov <asegizek@ece015.ece.local.cmu.edu>
2018-12-14 18:11:41 -0500

    fixed testvector typo

--- sha3_test.sv
+++ sha3_test.sv
@@ -17,15 +17,16 @@ module test;
     //keccak_f_fsm(clk, rst_l, rdy, st, out_rdy, out);
     integer i, j;
     initial begin
-        $monitor($time,,"st[0]=%h, st[1]=%h, out[0]=%h, out[1]=%h, out[2]=%h, out_rdy=%b", st[0][0], st[1][0], out[0][0], out[1][0], out[2][0], out_rdy);
+        $monitor($time,,"st[0]=%h, st[1]=%h, out[0]=%h, out[1][0]=%h, out[0][1]=%h, out_rdy=%b", st[0][0], st[1][0], out[0][0], out[1][0], out[0][1], out_rdy);
         for (i = 0; i < 5; i=i+1) begin
             for (j = 0; j < 5; j=j+1) begin
                 st[i][j] <= 0;
             end
         end
+
         st[0][0] <= 'h0D09DE907CCC2F9F;
         st[1][0] <= 'hEAC118977ECD876B;
-        st[2][0] <= 'hE95D2DFE1811B26C;
+        st[2][0] <= 'hE95D2DFC1811B26C;
         st[3][0] <= 'h109C1EACB65D7EF9;
         st[4][0] <= 'h6;
         st[1][3] <= 'h8000000000000000;
